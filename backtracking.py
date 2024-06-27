d9755ffdfb6796c4ec4fb133155a9a761c1f1640
Yokachii <smurfzbiturccouyon@gmail.com>
2024-06-27 10:38:33 +0200

    Time

--- backtracking.py
+++ backtracking.py
@@ -1,82 +1,63 @@
 import copy
+import time
 
-def genMap(int):
-    """
-    Génére un tableau de tableau correspondent a un echecier
-    """
-    array = []
-    for i in range(int):
-        array.append([])
-        for j in range(int):
-            array[i].append(0)
-    return array
+tps1 = time.time()
+solution_found = False
 
-# Liste des posibiliée accesible depuis une case sous forme de fonction
+def genMap(size):
+    """
+    Génère un tableau représentant un échiquier.
+    """
+    return [[0 for _ in range(size)] for _ in range(size)]
+
+# Liste des possibilités accessibles depuis une case sous forme de fonction
 posibiliteF = [
-    lambda x, y: [x+2,y+1],
-    lambda x, y: [x+2,y-1],
-    lambda x, y: [x-2,y+1],
-    lambda x, y: [x-2,y-1],
-    lambda x, y: [x+1,y+2],
-    lambda x, y: [x-1,y+2],
-    lambda x, y: [x+1,y-2],
-    lambda x, y: [x-1,y-2],
+    lambda x, y: [x+2, y+1],
+    lambda x, y: [x+2, y-1],
+    lambda x, y: [x-2, y+1],
+    lambda x, y: [x-2, y-1],
+    lambda x, y: [x+1, y+2],
+    lambda x, y: [x-1, y+2],
+    lambda x, y: [x+1, y-2],
+    lambda x, y: [x-1, y-2],
 ]
 
-def display(map):
+def display(board):
     """
-    Permet d'afficher un echecier a partir d'un tableau
+    Affiche un échiquier à partir d'un tableau.
     """
-    text = ""
-    for i in range(len(map)):
-        text+="\n"
-        for j in range(len(map[0])):
-            text+=str(map[i][j])+" "
-    return text
-        
-def countCase(map):
-    """
-    Permet de compter le nombre de case parcourie dans un chemain
-    """
-    c = 0
-    for i in range(len(map)):
-        for j in range(len(map[0])):
-            if map[i][j]>=1:
-                c += 1
-    return c
+    return "\n".join(" ".join(str(cell) for cell in row) for row in board)
 
-def cavalier(pos,map,n,taille=5,chemain=[]):
+def countCase(board):
     """
-    Fonction de backtracking cherchant des solution au problème
+    Compte le nombre de cases parcourues dans un chemin.
     """
+    return sum(cell >= 1 for row in board for cell in row)
 
-    tmpN = n
-    tmpMap = copy.deepcopy(map)
-    tmpChemain = copy.deepcopy(chemain)
+def cavalier(pos, board, n, taille=5, chemin=[]):
+    global solution_found, tps1
 
-    tmpMap[pos[1]][pos[0]]=n+1
-    tmpChemain.append(pos)
-    
-
-    if n>25 :
+    if solution_found:
         return
-    
-    tmpA = False
-    for case in [p(pos[0], pos[1]) for p in posibiliteF] :
 
-        x2 = case[0]
-        y2 = case[1]
+    tmpBoard = copy.deepcopy(board)
+    tmpChemin = chemin.copy()
 
-        if x2<=taille-1 and y2 <=taille-1 and x2 >=0 and y2 >=0:
+    tmpBoard[pos[1]][pos[0]] = n + 1
+    tmpChemin.append(pos)
 
+    if n == taille * taille - 1:
+        solution_found = True
+        print(display(tmpBoard))
+        print(tmpChemin)
+        tps2 = time.time()
+        print("Temp écouler: ", tps2 - tps1, "seconds")
+        return
 
-            if tmpMap[y2][x2] <1:
-                tmpA=True
-                cavalier([x2,y2],tmpMap,tmpN+1,taille,tmpChemain)
-                
-    if tmpA == False :
-        if n>=taille*taille-1 :
-            print(display(tmpMap))
-            print(chemain)
-    
-cavalier([2,2],genMap(5),0,5,[])
+    for case in (p(pos[0], pos[1]) for p in posibiliteF):
+        x2, y2 = case
+        if 0 <= x2 < taille and 0 <= y2 < taille and tmpBoard[y2][x2] == 0:
+            cavalier([x2, y2], tmpBoard, n + 1, taille, tmpChemin)
+
+intTaille = 5  # Taille de l'échiquier
+cavalier([2, 2], genMap(intTaille), 0, intTaille, [])
